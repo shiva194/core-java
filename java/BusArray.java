52860847694096637b499b2a4000ffff98440549
unknown <closefrends56@gmail.com>
2024-07-17 18:05:05 +0530

    file add to git

---
+++ java/BusArray.java
@@ -0,0 +1,17 @@
+class Addition{
+
+public static void main(String buses[]){
+		//System.out.println(128+125);
+				// System.out.println(58+98);
+						// System.out.println(526+481);
+								// System.out.println(984+235);
+								add(128,125);
+									add(254,16);
+										add(985,124);
+											add(874,654);							
+	}
+		public static void add(int num1,int num2){
+				System.out.println(num1+num2);
+	}
+	
+}
---
+++ java/Modulus.java
@@ -0,0 +1,14 @@
+class Modulus{
+
+
+public static void main(String modul[]){
+
+mod(98,54);
+mod(45,87);
+mod(25,64);
+	}
+public static void mod(int num1,int num2){
+
+ System.out.println(num1%num2);
+}
+}
